b9fffb2884069f13f32cc8bbb21e8d80eed535df
Ben Rasmussen <Benrasmussen11@gmail.com>
2018-02-22 20:42:42 -0600

    npm run build exercise to translate es6 to es5

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const $ = require('jquery');
+const data = require('./say-hello.js');
+const niceMessage = require('./say-hello.js');
+// test //
+$('body').html('Run es6 on older browsers and devices!!');
 
-$('body').html('Run es6 on older browsers and devices!!!');
-
-
+// test //
 const sayHello = name => {
     const greeting = 'Hello,  ' + name + '!';
     return greeting;
 };
-
 console.log(sayHello('ben'));
+
+// from data require //
+console.log(data.whichSideOfTheForce); // outputs "The Dark Side"
+
+// from niceMessage require //
+niceMessage(); // 'What a nice message!'
+
+
---
+++ src/say-hello.js
@@ -0,0 +1,12 @@
+const $ = require('jquery');
+
+const data = {
+    someNumber: 42,
+    whichSideOfTheForce: 'The Dark Side',
+    arnie: 'Get to da choppa!',
+};
+module.exports = data;
+
+// export to index.js //
+const helloThere = () => $('body').append(`<h1>What a  nice message</h1>`);
+module.exports = helloThere;
